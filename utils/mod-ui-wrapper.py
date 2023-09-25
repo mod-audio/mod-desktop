8cb5950c91566cbe7b4a0b33fe590e4adef67ee7
falkTX <falktx@falktx.com>
2023-09-25 16:11:14 +0200

    Start to set up CI
    Signed-off-by: falkTX <falktx@falktx.com>

--- utils/mod-ui-wrapper.py
+++ utils/mod-ui-wrapper.py
@@ -20,8 +20,21 @@ os.environ['MOD_DEVICE_WEBSERVER_PORT'] = '8888'
 os.environ['MOD_LOG'] = os.environ.get("MOD_LOG", '1')
 os.environ['MOD_KEY_PATH'] = os.path.join(DATA_DIR, 'keys')
 os.environ['MOD_HTML_DIR'] = os.path.join(ROOT, 'html')
+os.environ['MOD_USER_FILES_DIR'] = os.path.join(DATA_DIR, 'user-files')
 
-os.makedirs(DATA_DIR, exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'Audio Loops'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'Audio Recordings'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'Audio Samples'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'Audio Tracks'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'MIDI Clips'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'MIDI Songs'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'Reverb IRs'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'Speaker Cabinets IRs'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'Hydrogen Drumkits'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'SF2 Instruments'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'SFZ Instruments'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'Aida DSP Models'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'user-files', 'NAM Models'), exist_ok=True)
 
 # import webserver after setting up environment
 from mod import webserver
