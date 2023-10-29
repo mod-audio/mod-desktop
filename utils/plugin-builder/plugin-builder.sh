8961bf0d8c304fb25b88577875ca0a6dc772852e
falkTX <falktx@falktx.com>
2023-10-29 13:12:30 +0100

    Fix building macOS plugins
    Signed-off-by: falkTX <falktx@falktx.com>

--- utils/plugin-builder/plugin-builder.sh
+++ utils/plugin-builder/plugin-builder.sh
@@ -39,6 +39,6 @@ if [ "${MACOS}" -eq 1 ]; then
     export PATH="${PAWPAW_PREFIX}-host/bin:${PATH}"
 fi
 
-make -f utils/plugin-builder/plugin-builder.mk pkgname="${plugin}" ${MAKE_ARGS} PREFIX=/usr WITH_LTO=false VERBOSE=1
+make -f utils/plugin-builder/plugin-builder.mk pkgname="${plugin}" ${MAKE_ARGS} PREFIX=/usr NOOPT=true WITH_LTO=false VERBOSE=1
 
 # ---------------------------------------------------------------------------------------------------------------------
